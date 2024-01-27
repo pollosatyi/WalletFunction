0cad833158fe0d3955b5d7339bae4ed16b54924a
pollosatyi <pollosatyi@mail.ru>
2024-01-27 20:04:05 +0300

    Добавьте файлы проекта.

---
+++ Controllers/WalletFunctionController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WalletFunction.Controllers
+{
+    public class WalletFunctionController : Controller
+    {
+        // GET: WalletFunctionController
+        
+    }
+}
+//public ActionResult Index()
+//{
+//    return View();
+//}
+
+//// GET: WalletFunctionController/Details/5
+//public ActionResult Details(int id)
+//{
+//    return View();
+//}
+
+//// GET: WalletFunctionController/Create
+//public ActionResult Create()
+//{
+//    return View();
+//}
+
+//// POST: WalletFunctionController/Create
+//[HttpPost]
+//[ValidateAntiForgeryToken]
+//public ActionResult Create(IFormCollection collection)
+//{
+//    try
+//    {
+//        return RedirectToAction(nameof(Index));
+//    }
+//    catch
+//    {
+//        return View();
+//    }
+//}
+
+//// GET: WalletFunctionController/Edit/5
+//public ActionResult Edit(int id)
+//{
+//    return View();
+//}
+
+//// POST: WalletFunctionController/Edit/5
+//[HttpPost]
+//[ValidateAntiForgeryToken]
+//public ActionResult Edit(int id, IFormCollection collection)
+//{
+//    try
+//    {
+//        return RedirectToAction(nameof(Index));
+//    }
+//    catch
+//    {
+//        return View();
+//    }
+//}
+
+//// GET: WalletFunctionController/Delete/5
+//public ActionResult Delete(int id)
+//{
+//    return View();
+//}
+
+//// POST: WalletFunctionController/Delete/5
+//[HttpPost]
+//[ValidateAntiForgeryToken]
+//public ActionResult Delete(int id, IFormCollection collection)
+//{
+//    try
+//    {
+//        return RedirectToAction(nameof(Index));
+//    }
+//    catch
+//    {
+//        return View();
+//    }
+//}

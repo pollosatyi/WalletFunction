1edb616e58d27dc3ee44b867a86d55fd85f48452
pollosatyi <pollosatyi@mail.ru>
2024-01-27 20:04:44 +0300

    Первый коммит

--- WalletFunction/Program.cs
+++ WalletFunction/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Console.WriteLine("Программа для обмена");
+            while (true)
+            {
+                int inputNumber = 0;
+                Console.WriteLine("Введите 1 для добавления RUB на счет");
+                Console.WriteLine("Введите 0 для выхода");
+
+                string stringInputNumber = Console.ReadLine();
+                if (int.TryParse(stringInputNumber, out int number))
+                {
+                    inputNumber = number;
+                }
+                else
+                {
+                    Console.WriteLine("Это не число");
+                    continue;
+                }
+                if (inputNumber == 0)
+                {
+                    Console.WriteLine("Выход из программы");
+                }
+
+
+                switch (inputNumber)
+                {
+                }
     }
 }

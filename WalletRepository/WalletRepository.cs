9c4201164c1afe42ea194080d2706cb22b91d140
pollosatyi <pollosatyi@mail.ru>
2024-01-27 20:33:55 +0300

    DAL начало

---
+++ WalletRepository/WalletRepository.cs
@@ -0,0 +1,23 @@
+﻿using WalletFunction;
+
+namespace WalletRepository
+{
+    public class WalletRepository
+    {
+        public void Add(RubAccount rub)
+        {
+            string connectionString = "Data Source=DESKTOP-M2QA1DM\\SQLEXPRESS;Initial Catalog=walbase;User ID=User;Password=;Encrypt=False;Trusted_Connection=True";
+
+            // Создание подключения
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sqlExpression = $"INSERT INTO Task (Name, Description) VALUES ({task.Name}, {task.Description})";
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}

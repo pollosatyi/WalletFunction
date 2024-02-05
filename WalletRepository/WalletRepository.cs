838f48ccaf85d623438a1c91f5cf98d4109a830e
pollosatyi <pollosatyi@mail.ru>
2024-02-05 21:02:38 +0300

    попытка восстановить

--- WalletRepository/WalletRepository.cs
+++ WalletRepository/WalletRepository.cs
@@ -1,19 +1,20 @@
 ﻿using Microsoft.Data.SqlClient;
 using WalletDalContract;
 using WalletFunction;
+using static WalletFunction.Amount;
 
 namespace WalletRepositories
 {
     public class WalletRepository : IWalletRepository
     {
-        public void Add(RubBalance rub)
+        public void Add(AmountType type)
         {
             string connectionString = "Data Source=DESKTOP-M2QA1DM\\SQLEXPRESS;Initial Catalog=walbase;User ID=User;Password=;Encrypt=False;Trusted_Connection=True";
 
             // Создание подключения
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                string sqlExpression = $"INSERT INTO Wallets (rub) VALUES ({rub.Rub})";
+                string sqlExpression = $"INSERT INTO Wallets (rub) VALUES({type})";
                 connection.Open();
 
                 SqlCommand command = new SqlCommand(sqlExpression, connection);
